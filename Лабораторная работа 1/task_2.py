e40bff6727a58ac1d48ce5f6c1ff6e3da07e92ea
Houwy <64949295+HouwyTwitch@users.noreply.github.com>
2023-10-16 09:14:07 +0400

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,1 +1,14 @@
-print("Количество книг, помещающихся на дискету:", int(1.44/(4*25*50*100/1024/1024)))
+# TODO Найдите количество книг, которое можно разместить на дискете
+
+floppy_drive_size_mb = 1.44
+pages_in_each_book = 100
+lines_on_the_page = 50
+symbols_in_the_line = 25
+size_of_symbol = 4
+kb = 1024  # Размер килобайта в байтах
+mb = 1024
+floppy_drive_size_bytes = floppy_drive_size_mb * mb * kb
+books_num = int(floppy_drive_size_bytes / (pages_in_each_book * lines_on_the_page *
+                                           symbols_in_the_line * size_of_symbol))
+
+print("Количество книг, помещающихся на дискету:", books_num)
